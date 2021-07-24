1d96356f08e8a5d0ef5d2290b631081937d17f2c
teerawat prarom <teewat@yahoo.com>
2021-07-25 00:13:35 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const mname = [ 'Lisa', 'Kaitlin', 'Jan' ];
+const msg = [];
+function writeCards(mname,word){
+    for (let i = 0 ; i < mname.length; i++){
+        msg[i] = 'Thank you, '+mname[i]+', for the wonderful '+word+' gift!';
+        console.log(msg[i]);
+        
+    }
+    return msg
+}
+
+function countDown(n){
+    while(n >= 0){
+        console.log(n);
+        n-- ;
+    }
+}
